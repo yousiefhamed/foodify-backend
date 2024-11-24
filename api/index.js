624d48a01b3ae7db5084adfbfcb95f93df470425
Youssef Hamed <yousiefhamed1@gmail.com>
2024-11-24 14:55:29 +0200

    fixing db connection

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
 // Connect to MongoDB
-import connectDB from "../config/db.js";
+const connectDB = require("../config/db");
 
 dotenv.config();
 
@@ -17,11 +17,16 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-try {
-  const conn = await connectDB();
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
-} catch (error) {
-  console.log(error);
-}
+
+const serverListener = async () => {
+  try {
+    const conn = await connectDB();
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+serverListener();
